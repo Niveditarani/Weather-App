a9b7ee2c0d4089e4333997e88c897132b8f9d7b6
Nivedita Rani <Nivedita.rani19@gmail.com>
2021-02-23 18:00:00 +0100

    made weather card responsive

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-//    "apiKey": "2e2402e031527e1ced45dbbe88d99f0a"
-    
-//select elements
-const locationElement = document.querySelector(".location p");
-const descriptionElement = document.querySelector(".temp-description p");
+//api key: 2e2402e031527e1ced45dbbe88d99f0a
+
+// SELECT ELEMENTS
+const searchElement = document.querySelector(".search-btn");
+const cityElement = document.querySelector(".city");
+const dateElement = document.querySelector(".date");
+const descElement = document.querySelector(".temp-description p");
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temp-value p");
+const feelsLikeElement = document.querySelector(".feels-like span");
 const notificationElement = document.querySelector(".notification");
+const windElement = document.querySelector(".wind-speed span");
+const humidityElement = document.querySelector(".humidity span");
 
-//weather object- App data
-const weather = {};
+var input = document.getElementById("search");
+
+let latitude = 0.0;
+let longitude = 0.0;
+
+input.addEventListener("keyup", function(event){
+    let city;
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        city = input.value;
+        getSearchWeather(city)
+        console.log(city)
+    }
+})
+// App data
+const weather = {}
+
 weather.temperature = {
-    unit: "celsius"
+    unit : "celsius"
 }
-
-//App const & Vars
+// APP CONSTS AND VARS
 const KELVIN = 273;
-
-//API key
+// API KEY
 const key = "2e2402e031527e1ced45dbbe88d99f0a";
 
 //check if browser support geolocation
@@ -38,7 +58,37 @@ function showError(error){
     notificationElement.style.display= "block";
     notificationElement.innerHTML = `<p>${error.message}</p>`;
 }
-//get weather from API provider
+searchElement.addEventListener("click", function(event){
+    console.log("hi")
+if(input.value === "")
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+    //  getWeather(latitude, longitude);
+else
+    getSearchWeather(input.value);
+})
+function getSearchWeather(city){
+   // var city = input.value;
+
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then (function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.feels= Math.floor(data.main.feels_like- KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+            weather.wind = data.wind.speed;
+            weather.humidity = data.main.humidity;
+        })
+        .then (function(){
+            displayWeather();
+        });
+}
 function getWeather(latitude, longitude){
     let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
     fetch(api)
@@ -48,24 +98,41 @@ function getWeather(latitude, longitude){
         })
         .then (function(data){
             weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.feels= Math.floor(data.main.feels_like- KELVIN);
             weather.description = data.weather[0].description;
             weather.iconId = data.weather[0].icon;
             weather.city = data.name;
             weather.country = data.sys.country;
+            weather.wind = data.wind.speed;
+            weather.humidity = data.main.humidity;
         })
         .then (function(){
             displayWeather();
         });
 }
-
 //display weather to UI
 function displayWeather(){
     iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
     tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-    descriptionElement.innerHTML = weather.description;
-    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+    descElement.innerHTML = weather.description;
+    cityElement.innerHTML = `${weather.city}, ${weather.country} `;
+    feelsLikeElement.innerHTML = ` ${weather.feels}`;
+    windElement.innerHTML = ` ${weather.wind}`;
+    humidityElement.innerHTML = ` ${weather.humidity}`;
+    let now = new Date();
+    dateElement.innerHTML = dateBuilder(now);
 }
-
+function dateBuilder (d) {
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+  
+    return `${day}, ${date} ${month} ${year}`;
+  }
 //convert C to F
 function celsiusToFahrenheit(temperature){
     return (temperature * 9/5) +32;
