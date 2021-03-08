bd4710faa9662d96b707b633e75d59ee880d7235
Nivedita Rani <Nivedita.rani19@gmail.com>
2021-03-08 10:20:44 +0100

    added feature of toggle-tab to get hourly and daily forecast

---
+++ tabScript.js
@@ -0,0 +1,20 @@
+function openForecast(evt, tabName){
+    //declare all variables
+    var i, tabcontent, tablinks;
+
+//get all elements with class="tabcontent" & hide them
+tabcontent = document.getElementsByClassName("tabcontent");
+for(i=0; i<tabcontent.length; i++){
+    tabcontent[i].style.display = "none";
+}
+//get all elements with class="tablinks" & remove the class "active"
+tablinks = document.getElementsByClassName("tablinks");
+for(i=0; i<tablinks.length; i++){
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+}
+//show the current tab & add an "active" class to the button that opened the tab
+document.getElementById(tabName).style.display = "block";
+evt.currentTarget.className += " active";
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
